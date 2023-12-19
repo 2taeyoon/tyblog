172cecc88fc362a8007f7589c0b915821467de7b
2taeyoon <2tae@kakao.com>
2023-12-19 19:03:47 +0900

    setting: robot, favicon 셋팅

--- src/components/layout/Aside.tsx
+++ src/components/layout/Aside.tsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 export default function Aside() {
 	return (
-		<aside>
-			
+		<aside className="aside">
+			<div className="aside_wrap">
+				<div className="profile_wrap">
+					<div className="profile"></div>
+					<div className="sitename">TYCODE</div>
+					<div className="nickname">2taeyoon</div>
+				</div>
+			</div>
 		</aside>
 	)
 }
