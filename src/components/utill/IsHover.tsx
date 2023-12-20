5a94ee7954061a4741430256aab7c79395f7209f
2taeyoon <2tae@kakao.com>
2023-12-21 01:26:50 +0900

    utill: 호버시 클래스 네임 기능 컴포넌트 구현

---
+++ src/components/utill/IsHover.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import { IsHoverProps } from "../../types/props";
+
+export default function IsHover({ children, baseClass, hoverClass }: IsHoverProps) {
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+    <div
+      className={`${baseClass} ${isHovered ? hoverClass : ""}`}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      {children}
+    </div>
+  );
+}
