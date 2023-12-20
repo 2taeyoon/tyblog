ead69a26524dd668968616e09492ba781ded90a0
2taeyoon <2tae@kakao.com>
2023-12-21 01:28:54 +0900

    chor: Aside 퍼블리싱 중..

--- src/components/layout/Aside.tsx
+++ src/components/layout/Aside.tsx
@@ -5,9 +5,17 @@ export default function Aside() {
 		<aside className="aside">
 			<div className="aside_wrap">
 				<div className="profile_wrap">
-					<div className="profile"></div>
-					<div className="sitename">TYCODE</div>
+					<div className="profile">
+						<img src="./images/profile.png" alt="profile" />
+					</div>
+					<div className="sitename">TYCODESIGN</div>
 					<div className="nickname">2taeyoon</div>
+				</div>
+				<div className="another_site">
+					<a href="https://notefolio.net/2taeyoon" target="_blank" className="notefolio"></a>
+					<a href="https://loud.kr/m/2taeyoon" target="_blank" className="loud"></a>
+					<a href="https://velog.io/@2taeyoon" target="_blank" className="velog"></a>
+					<a href="https://github.com/2taeyoon" target="_blank" className="github"></a>
 				</div>
 			</div>
 		</aside>
--- src/types/props.ts
+++ src/types/props.ts
@@ -1,7 +1,15 @@
+import { ReactNode } from 'react';
+
 export type MetaProps = {
 	title: string;
 	keywords: string;
 	description: string;
 	ogTitle: string;
 	ogDescription: string;
+}
+
+export type IsHoverProps = {
+	children: ReactNode;
+  baseClass: string;
+  hoverClass: string;
 }
