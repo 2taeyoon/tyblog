96d153fa10196c2af188129acf8f14e031173661
2taeyoon <2tae@kakao.com>
2023-12-21 19:32:44 +0900

    feat: list 컴포넌트 추가

---
+++ src/components/list/ListLink.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+import { ListLinkProps } from "../../types/props";
+
+export default function ListLink({ linkTo, title, baseClass }: ListLinkProps) {
+  return (
+    <li>
+      <Link to={linkTo} className={baseClass}>{title}</Link>
+    </li>
+  );
+}
--- src/types/props.ts
+++ src/types/props.ts
@@ -1,15 +1,27 @@
 import { ReactNode } from 'react';
 
-export type MetaProps = {
+// 메타 태그 START!
+export interface MetaProps {
 	title: string;
 	keywords: string;
 	description: string;
 	ogTitle: string;
 	ogDescription: string;
 }
+// 메타 태그 END!
 
-export type IsHoverProps = {
+// 호버시 클래스 추가 START!
+export interface IsHoverProps {
 	children: ReactNode;
   baseClass: string;
   hoverClass: string;
 }
+// 호버시 클래스 추가 END!
+
+// 링크 컴포넌트 START!
+export interface ListLinkProps {
+	linkTo: string;
+	title: string;
+	baseClass?: string;
+} 
+// 링크 컴포넌트 END!
