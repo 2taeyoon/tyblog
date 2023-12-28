3816f9c3bc7560c36c6b8cf55667a1fa863e030a
2taeyoon <2tae@kakao.com>
2023-12-28 17:31:55 +0900

    chor: 자잘한 CSS 수정

--- src/components/layout/Aside.tsx
+++ src/components/layout/Aside.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import ListLink from "../list/ListLink";
-// import { navLinks } from "../../data/List";
+import { asideLinks, categories } from "../../data/List";
+import CategoryLinks from "../ui/CategoryLinks";
 
 export default function Aside() {
   return (
@@ -18,38 +19,17 @@ export default function Aside() {
           <div className="nickname">2taeyoon</div>
         </div>
         <ul className="another_site">
-					<ListLink
-						linkTo="https://notefolio.net/2taeyoon"
-						baseClass="notefolio"
-						target="_blank"
-						rel="noreferrer noopener"
-						ariaLabel="NoteFolio 프로필 보기"
-						image="./images/notefolio_logo.png"
-					></ListLink>
-					<ListLink
-						linkTo="https://loud.kr/m/2taeyoon"
-						baseClass="loud"
-						target="_blank"
-						rel="noreferrer noopener"
-						ariaLabel="Loud 프로필 보기"
-						image="./images/loud_logo.png"
-					></ListLink>
-					<ListLink
-						linkTo="https://velog.io/@2taeyoon"
-						baseClass="velog"
-						target="_blank"
-						rel="noreferrer noopener"
-						ariaLabel="velog 프로필 보기"
-						image="./images/velog_logo.png"
-					></ListLink>
-					<ListLink
-						linkTo="https://github.com/2taeyoon"
-						baseClass="github"
-						target="_blank"
-						rel="noreferrer noopener"
-						ariaLabel="Github 프로필 보기"
-						image="./images/github_logo.png"
-					></ListLink>
+					{asideLinks.map((link, index) => (
+						<ListLink
+							key={index}
+							linkTo={link.linkTo}
+							baseClass={link.baseClass}
+							target="_blank"
+							rel="noreferrer noopener"
+							ariaLabel={link.ariaLabel}
+							image={link.image}
+						/>
+					))}
         </ul>
         <nav className="nav">
           <Link
@@ -58,51 +38,14 @@ export default function Aside() {
           >
             ALL POST
           </Link>
-          <div className="nav_wrap">
-            <div className="category">Portfolio</div>
-            <ul>
-              <ListLink
-                baseClass="item"
-                linkTo="/designportfolio"
-                title="Design Portfolio"
-              />
-              <ListLink
-                baseClass="item"
-                linkTo="/codeportfolio"
-                title="Code Portfolio"
-              />
-            </ul>
-            <div className="category">Study</div>
-            <ul>
-              <ListLink
-                baseClass="item"
-                linkTo="/designstudy"
-                title="Design Study"
-              />
-              <ListLink
-                baseClass="item"
-                linkTo="/codestudy"
-                title="Code Study"
-              />
-              <ListLink
-                baseClass="item"
-                linkTo="/etcstudy"
-                title="Etc Study"
-              />
-            </ul>
-          </div>
-          {/* <div>
-						{navLinks.map((section) => (
-							<div key={section.category}>
-								<div>{section.category}</div>
-								<ul>
-									{section.items.map((item) => (
-										<ListLink key={item.linkTo} linkTo={item.linkTo} title={item.title} />
-									))}
-								</ul>
-							</div>
-						))}
-					</div> */}
+					{categories.map((category, index) => (
+						<CategoryLinks
+							key={index}
+							category={category.name}
+							categoryClass="category"
+							links={category.links}
+						/>
+					))}
         </nav>
       </div>
     </aside>
