b52df34f978277e1d3224e80f59cdeb9fa1c2c28
2taeyoon <2tae@kakao.com>
2023-12-28 10:09:47 +0900

    delete: 모든 파일 삭제

--- src/pages/RouteApp.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import RouteApp from "./RouteApp";
-
-test("renders learn react link", () => {
-  render(<RouteApp />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
--- src/pages/study/EtcStudy.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function EtcStudy() {
-  return <div className="common_pd">EtcStudy</div>;
-}
