5d448fb9aa6464202dfc8d45b7f3a02e90f64870
2taeyoon <2tae@kakao.com>
2024-01-05 17:01:08 +0900

    chor: 파일 이름 변경

--- src/data/slideData.ts
+++ src/data/slideData.ts
@@ -1,10 +1,10 @@
 export const SlidesData: string[] = [
-  './images/slide/slide_image1.webp',
-  './images/slide/slide_image2.webp',
-  './images/slide/slide_image3.webp',
-	'./images/slide/slide_image4.webp',
-	'./images/slide/slide_image5.webp',
-	'./images/slide/slide_image6.webp',
-	'./images/slide/slide_image7.webp',
-	'./images/slide/slide_image8.webp',
+  './images/slide/slide_image1.png',
+  './images/slide/slide_image2.png',
+  './images/slide/slide_image3.png',
+	'./images/slide/slide_image4.png',
+	'./images/slide/slide_image5.png',
+	'./images/slide/slide_image6.png',
+	'./images/slide/slide_image7.png',
+	'./images/slide/slide_image8.png',
 ];
