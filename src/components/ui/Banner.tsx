14c7cb430a17cc860a8f600d83c21bf0c7fd6315
2taeyoon <2tae@kakao.com>
2024-01-22 01:11:45 +0900

    component: 배너 컴포넌트 생성

---
+++ src/components/ui/Banner.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { CardProps } from "../../types/props"
+
+export default function Banner({CardFind} : {CardFind: CardProps}) {
+	return (
+		<div className="banner">
+			<div className="banner_image" style={{ background: `url('${CardFind?.image}') center center / cover` }}></div>
+			<div className="banner_info">
+				<div className="card_title">{CardFind?.title}</div>
+				<div className="card_bottom_profile">
+					<div className="card_nickname">{CardFind?.nickname}</div>
+					<div className="card_date">{CardFind?.date}</div>
+				</div>
+				{ CardFind?.hashs ? (
+					<div className="card_hash_wrap">
+						{ CardFind.hashs.map((hash, hashIndex) => (
+							<div key={hashIndex} className="card_hash">
+								<div className="hash_text">{hash.name}</div>
+							</div>
+						))}
+					</div>	) : null}
+			</div>
+		</div>
+	)
+}
