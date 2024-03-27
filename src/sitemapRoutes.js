c759fb5e1abdf351ac8a9ead466b3491241374bd
2taeyoon <2tae@kakao.com>
2024-03-27 17:14:48 +0900

    chor: 사이트맵 테스트 중

---
+++ src/sitemapRoutes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route } from 'react-router';
+
+export default (
+    <Route>
+        <Route path="/"  />
+        <Route path="/designportfolio" />
+        <Route path="/publishingportfolio" />
+        <Route path="/designstudy" />
+        <Route path="/publishingstudy" />
+        <Route path="/etcstudy" />
+    </Route>
+);
