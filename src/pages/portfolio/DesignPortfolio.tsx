ecae56cbf87109f5e6dd6af7483dd897c2d944e4
2taeyoon <2tae@kakao.com>
2024-03-29 13:55:54 +0900

    chor: 3번 포트폴리오 추가 및 자잘한 css 수정

--- src/pages/portfolio/DesignPortfolio.tsx
+++ src/pages/portfolio/DesignPortfolio.tsx
@@ -1,31 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import CommonHelmet from "../../components/utill/CommonHelmet";
 import DesignPortfolioCard from "../../data/designPortfolioData.json";
 import Card from "../../components/list/Card";
 import SliderFade from "../../components/ui/SliderFade";
-import { CardProps } from "../../types/props";
-import Hashs from "../../components/utill/Hashs";
 import Saying from "../../components/ui/Saying";
 
 export default function DesignPortfolio() {
-  const [selectedHash, setSelectedHash] = useState<string | null>(null); // 해시태그를 상태로 관리
-  const [filteredCards, setFilteredCards] = useState<CardProps[]>([]); // 필터링 카드를 상태로 관리
-
-	// 선택된 해시태그에 따라 카드를 필터링하는 함수 START!
-  useEffect(() => {
-    if (selectedHash) { // 카드 중 해시태그가 일치하는 카드만 필터링합니다.
-      const newFilteredCards = DesignPortfolioCard.cards.filter(card =>
-        card.hashs.some(hash => hash.name === selectedHash)
-      );
-      setFilteredCards(newFilteredCards);
-    } else { // 선택된 해시태그가 없다면 모든 카드를 표시
-      setFilteredCards(DesignPortfolioCard.cards);
-    }
-  }, [selectedHash])
-
-	// 카드에서 중복되지 않은 해시태그들만 추출하여 배열로 생성
-	const uniqueHashs = Array.from(new Set(DesignPortfolioCard.cards.flatMap(card => card.hashs.map(hash => hash.name))));
-
   return (
     <>
       <CommonHelmet
@@ -41,11 +21,10 @@ export default function DesignPortfolio() {
 				<div className="category_wrap">
 					<div className="category_text">Design Portfolio</div>
 				</div>
-				<Hashs selectedHash={selectedHash} setSelectedHash={setSelectedHash} uniqueHashs={uniqueHashs} sessionName="DesignPortfolioHashs"/>
 			</div>
 			<div className="common_wrap">
 				<div className="card_wrap">
-					{filteredCards.map(card => (
+					{DesignPortfolioCard.cards.map(card => (
 						<Card key={card.title} cards={[card]} />
 					))}
 				</div>
--- src/pages/portfolio/PublishingPortfolio.tsx
+++ src/pages/portfolio/PublishingPortfolio.tsx
@@ -1,31 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import CommonHelmet from "../../components/utill/CommonHelmet";
 import SliderFade from "../../components/ui/SliderFade";
 import PublishingPortfolioCard from "../../data/publishingPortfolioData.json";
 import Card from "../../components/list/Card";
-import { CardProps } from "../../types/props";
-import Hashs from "../../components/utill/Hashs";
 import Saying from "../../components/ui/Saying";
 
 export default function PublishingPortfolio() {
-  const [selectedHash, setSelectedHash] = useState<string | null>(null); // 해시태그를 상태로 관리
-  const [filteredCards, setFilteredCards] = useState<CardProps[]>([]); // 필터링 카드를 상태로 관리
-
-	// 선택된 해시태그에 따라 카드를 필터링하는 함수 START!
-  useEffect(() => {
-    if (selectedHash) { // 카드 중 해시태그가 일치하는 카드만 필터링합니다.
-      const newFilteredCards = PublishingPortfolioCard.cards.filter(card =>
-        card.hashs.some(hash => hash.name === selectedHash)
-      );
-      setFilteredCards(newFilteredCards);
-    } else { // 선택된 해시태그가 없다면 모든 카드를 표시
-      setFilteredCards(PublishingPortfolioCard.cards);
-    }
-  }, [selectedHash])
-
-	// 카드에서 중복되지 않은 해시태그들만 추출하여 배열로 생성
-	const uniqueHashs = Array.from(new Set(PublishingPortfolioCard.cards.flatMap(card => card.hashs.map(hash => hash.name))));
-
   return (
     <>
       <CommonHelmet
@@ -41,11 +21,10 @@ export default function PublishingPortfolio() {
 				<div className="category_wrap">
 					<div className="category_text">Publishing Portfolio</div>
 				</div>
-				<Hashs selectedHash={selectedHash} setSelectedHash={setSelectedHash} uniqueHashs={uniqueHashs} sessionName="PublishingPortfolioHashs"/>
 			</div>
 			<div className="common_wrap">
 				<div className="card_wrap">
-					{filteredCards.map(card => (
+					{PublishingPortfolioCard.cards.map(card => (
 						<Card key={card.title} cards={[card]} />
 					))}
 				</div>
