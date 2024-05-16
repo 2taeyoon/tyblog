5ebd384a850d50b6b72bd8cfb256f8018a181ce6
2taeyoon <2tae@kakao.com>
2024-05-16 16:09:26 +0900

    git reset

--- webpack.config.js
+++ webpack.config.js
@@ -68,6 +68,7 @@ module.exports = (argv) => {
 					{ from: './public/favicon', to: 'favicon' },
 					{ from: './public/manifest.json', to: 'manifest.json' },
 					{ from: './public/robots.txt', to: 'robots.txt' },
+					{ from: './public/sitemap.xml', to: 'sitemap.xml' },
 				],
 			}),
       new CleanWebpackPlugin()
