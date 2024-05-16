30cc9a0aa1e96a2e7563eff8ade52f3f48c8b235
2taeyoon <2tae@kakao.com>
2024-05-16 16:37:47 +0900

    reset

--- src/components/utill/CommonHelmet.tsx
+++ src/components/utill/CommonHelmet.tsx
@@ -2,15 +2,14 @@ import React from "react";
 import { Helmet } from "react-helmet-async";
 import { MetaProps } from "../../types/props";
 
-const CommonHelmet = ({
-  title,
-  keywords,
-  description,
-  ogTitle,
-  ogDescription,
-	ogImage,
-	ogURL
-}: MetaProps) => (
+/*
+<meta name="robots" content="index,nofollow"> // 색인 대상O, 페이지 내 링크 수집X
+<meta name="robots" content="noindex,follow"> // 색인 대상X, 페이지 내 링크 수집O
+<meta name="robots" content="noindex,nofollow"> // 색인 대상X, 페이지 내 링크 수집X
+<meta name="googlebot" content="noindex, nofollow"> // 구글에서만 사이트 노출X
+*/
+
+const CommonHelmet = ({ title, keywords, description, ogTitle, ogDescription,	ogImage, ogURL }: MetaProps) => (
   <Helmet>
     <title>{title}</title>
     <meta name="keywords" content={keywords}/>
@@ -23,7 +22,6 @@ const CommonHelmet = ({
 		<meta property="og:image:height" content="630"/>
 		<meta property="og:type" content="website"/>
 		<meta property="og:site_name" content="이태윤의 블로그형 포트폴리오 사이트입니다."/>
-		
   </Helmet>
 );
 
