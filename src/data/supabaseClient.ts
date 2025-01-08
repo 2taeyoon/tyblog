9b53ed7eacc362203a6ce1a0643729257a072a06
2taeyoon <2tae@kakao.com>
2025-01-09 07:03:09 +0900

    error: 배포 후 환경변수 오류..

--- src/data/supabaseClient.ts
+++ src/data/supabaseClient.ts
@@ -3,4 +3,8 @@ import { createClient, SupabaseClient } from '@supabase/supabase-js';
 const supabaseUrl: string = process.env.REACT_APP_SUPABASE_URL!;
 const supabaseAnonKey: string = process.env.REACT_APP_SUPABASE_ANON!;
 
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("supabaseUrl and supabaseAnonKey are required.");
+}
+
 export const supabase: SupabaseClient = createClient(supabaseUrl, supabaseAnonKey);
