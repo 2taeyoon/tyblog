2a70d18bfcaec4a170ed22db65dd11bef3191bfe
2taeyoon <2tae@kakao.com>
2025-01-21 14:31:49 +0900

    feat: Home.tsx에서 Card 클릭 시 Card type에 따른 동적 컴포넌트 추가

---
+++ src/components/list/Card2.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Mapping } from "../../types/props"
+import { Link } from "react-router-dom";
+
+export default function Card({ cards }: Mapping) {
+	return (
+		<>
+			{ cards.map((card, index) => (
+				<Link to={`${card.type}/${card.title?.replace(/\s+/g, '-')}`} key={index} className="card">
+					<div className="card_top">
+						<div className="card_img" style={{ background: `url('${card.image}') center center / cover` }}>
+							<div className="card_badge_wrap">
+								{ card.badge ?
+									<div className="card_badge">
+										<div>{card.badge}</div>
+									</div> : null
+								}
+								{ card.badge2 ?
+									<div className="card_badge">
+										<div>{card.badge2}</div>
+									</div> : null
+								}
+							</div>
+						</div>
+					</div>
+					<div className="card_bottom">						
+						<div className="card_bottom_title">{card.title}</div>
+						<div className="card_date">{card.date}</div>
+						{ card.subTitle? <div className="card_bottom_sub_title">{card.subTitle}</div> : null }
+					</div>
+				</Link>
+			))}
+	</>
+	)
+}
