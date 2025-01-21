d0a4ebadea071d604b7ccd30c85a94fb65d79979
2taeyoon <2tae@kakao.com>
2025-01-21 18:25:08 +0900

    feat: JSON 배열 합치기, 날짜순 배열 재정렬

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,25 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import CommonHelmet from "../../components/utill/CommonHelmet";
 import SliderFade from "../../components/ui/SliderFade";
+import designStudyData from "../../data/designStudyData.json";
+import publishingStudyData from "../../data/publishingStudyData.json";
+import etcStudyData from "../../data/etcStudyData.json";
+import CardPagination from "../../components/utill/CardPagination";
+import Saying from "../../components/ui/Saying";
 
 console.log(process.env.REACT_APP_SUPABASE_ANON, "REACT_APP_SUPABASE_ANON");
 console.log(process.env.REACT_APP_SUPABASE_URL, "EACT_APP_SUPABASE_URL");
 
 export default function Home() {
+
+	const [currentPage, setCurrentPage] = useState(0);
+
+	const combinedData = [
+    ...designStudyData.cards.map(card => ({ ...card, type: "ds" })),
+    ...etcStudyData.cards.map(card => ({ ...card, type: "es" })),
+    ...publishingStudyData.cards.map(card => ({ ...card, type: "ps" })),
+  ];
+
+	// 최신 날짜순으로 정렬 START!
+	const sortedData = combinedData.sort((a, b) => {
+		const dateA:any = new Date(a.sortDate || "2024-01-01");
+		const dateB:any = new Date(b.sortDate || "2024-01-01");
+		return dateB - dateA; // 최신 날짜가 먼저 오도록 정렬
+	});
+	// 최신 날짜순으로 정렬 END!
+
   return (
     <>
       <CommonHelmet
-        title="이태윤 포트폴리오"
-        description="이태윤의 개인 공간이자 포트폴리오 사이트"
-        ogTitle="이태윤 포트폴리오"
-        ogDescription="이태윤의 개인 공간이자 포트폴리오 사이트"
+        title="2taeyoon의 개인 블로그"
+        description="개인 공간이자 포트폴리오 사이트"
+        ogTitle="포트폴리오"
+        ogDescription="개인 공간이자 포트폴리오 사이트"
         keywords="2taeyoon,이태윤,포트폴리오"
 				ogImage="https://www.2taeyoon.com/favicon/favicon-512x512.png"
 				ogURL="https://www.2taeyoon.com/"
       />
-      <SliderFade typingText="1년차 퍼블리셔 이태윤입니다." typingText2="나만의 공간을 만들고 싶어 제작한 웹사이트입니다."/>
+      <SliderFade typingText="" typingText2=""/>
+			<div className="common_wrap pd_none_col">
+				<Saying sessionName="Home"/>
+			</div>
       <div className="common_wrap">
-				여기는 이력서와 자기소개서가 들어가는 페이지 ing..
+				<CardPagination filteredCards={sortedData} sessionName="HomeHashs" currentPage={currentPage} setCurrentPage={setCurrentPage}/>
 			</div>
     </>
   );
