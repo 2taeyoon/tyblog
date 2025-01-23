78cb6305968697195025c5544571747b5bc81d6f
2taeyoon <2tae@kakao.com>
2025-01-23 10:37:24 +0900

    feat: json 통합 및 카드 아이템 전체 출력

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import CommonHelmet from "../../components/utill/CommonHelmet";
 import SliderFade from "../../components/ui/SliderFade";
 import designStudyData from "../../data/designStudyData.json";
@@ -6,45 +6,65 @@ import publishingStudyData from "../../data/publishingStudyData.json";
 import etcStudyData from "../../data/etcStudyData.json";
 import CardPagination from "../../components/utill/CardPagination";
 import Saying from "../../components/ui/Saying";
-
-console.log(process.env.REACT_APP_SUPABASE_ANON, "REACT_APP_SUPABASE_ANON");
-console.log(process.env.REACT_APP_SUPABASE_URL, "EACT_APP_SUPABASE_URL");
+import Hashs from "../../components/utill/Hashs";
+import { CardProps } from "../../types/props";
 
 export default function Home() {
-
+	const [selectedHash, setSelectedHash] = useState<string | null>(null); // 해시태그를 상태로 관리
+	const [filteredCards, setFilteredCards] = useState<CardProps[]>([]);
 	const [currentPage, setCurrentPage] = useState(0);
 
-	const combinedData = [
-    ...designStudyData.cards.map(card => ({ ...card, type: "ds" })),
-    ...etcStudyData.cards.map(card => ({ ...card, type: "es" })),
-    ...publishingStudyData.cards.map(card => ({ ...card, type: "ps" })),
-  ];
+  const combinedData = useMemo(() => {
+    return [
+      ...designStudyData.cards.map(card => ({ ...card, type: "ds" })),
+      ...etcStudyData.cards.map(card => ({ ...card, type: "es" })),
+      ...publishingStudyData.cards.map(card => ({ ...card, type: "ps" })),
+    ];
+  }, []);
 
-	// 최신 날짜순으로 정렬 START!
-	const sortedData = combinedData.sort((a, b) => {
-		const dateA:any = new Date(a.sortDate || "2024-01-01");
-		const dateB:any = new Date(b.sortDate || "2024-01-01");
-		return dateB - dateA; // 최신 날짜가 먼저 오도록 정렬
-	});
-	// 최신 날짜순으로 정렬 END!
+	// 선택된 해시태그에 따라 카드를 필터링하는 함수 START!
+	useEffect(() => {
+		let filtered = combinedData;
+
+		if (selectedHash) {
+      // 선택된 해시태그에 따라 필터링
+      filtered = combinedData.filter(card =>
+        card.hashs.some(hash => hash.name === selectedHash)
+      );
+    }
+
+		// 최신순으로 정렬
+		const sorted = filtered.sort((a, b) => {
+			const dateA = new Date(a.sortDate || "2024-01-01").getTime();
+			const dateB = new Date(b.sortDate || "2024-01-01").getTime();
+			return dateB - dateA;
+		});
+
+		setFilteredCards(sorted);
+	}, [selectedHash, combinedData])
+	// 선택된 해시태그에 따라 카드를 필터링하는 함수 END!
+
+	// 카드에서 중복되지 않은 해시태그들만 추출하여 배열로 생성
+	const uniqueHashs = Array.from(new Set(combinedData.flatMap(card => card.hashs.map(hash => hash.name))));
 
   return (
     <>
       <CommonHelmet
         title="2taeyoon의 개인 블로그"
         description="개인 공간이자 포트폴리오 사이트"
-        ogTitle="포트폴리오"
+        ogTitle="개인 블로그"
         ogDescription="개인 공간이자 포트폴리오 사이트"
-        keywords="2taeyoon,이태윤,포트폴리오"
+        keywords="2taeyoon,포트폴리오,개인 블로그"
 				ogImage="https://www.2taeyoon.com/favicon/favicon-512x512.png"
 				ogURL="https://www.2taeyoon.com/"
       />
       <SliderFade typingText="" typingText2=""/>
 			<div className="common_wrap pd_none_col">
 				<Saying sessionName="Home"/>
+				<Hashs selectedHash={selectedHash} setSelectedHash={setSelectedHash} uniqueHashs={uniqueHashs} sessionName="Home" setCurrentPage={setCurrentPage}/>
 			</div>
       <div className="common_wrap">
-				<CardPagination filteredCards={sortedData} sessionName="HomeHashs" currentPage={currentPage} setCurrentPage={setCurrentPage}/>
+				<CardPagination filteredCards={filteredCards} sessionName="Home" currentPage={currentPage} setCurrentPage={setCurrentPage}/>
 			</div>
     </>
   );
