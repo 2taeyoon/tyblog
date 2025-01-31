8499c9537003fbfce7a80ab1d8f9f57078fd751b
2taeyoon <2tae@kakao.com>
2025-02-01 07:53:26 +0900

    chore: ogType 추가가

--- src/components/utill/CommonHelmet.tsx
+++ src/components/utill/CommonHelmet.tsx
@@ -2,14 +2,7 @@ import React from "react";
 import { Helmet } from "react-helmet-async";
 import { MetaProps } from "../../types/props";
 
-/*
-<meta name="robots" content="index,nofollow"> // 색인 대상O, 페이지 내 링크 수집X
-<meta name="robots" content="noindex,follow"> // 색인 대상X, 페이지 내 링크 수집O
-<meta name="robots" content="noindex,nofollow"> // 색인 대상X, 페이지 내 링크 수집X
-<meta name="googlebot" content="noindex, nofollow"> // 구글에서만 사이트 노출X
-*/
-
-const CommonHelmet = ({ title, keywords, description, ogTitle, ogDescription,	ogImage, ogURL }: MetaProps) => (
+const CommonHelmet = ({ title, keywords, description, ogTitle, ogDescription,	ogURL, ogImage, ogType }: MetaProps) => (
   <Helmet>
     <title>{title}</title>
     <meta name="keywords" content={keywords}/>
@@ -18,10 +11,10 @@ const CommonHelmet = ({ title, keywords, description, ogTitle, ogDescription,	og
     <meta property="og:description" content={ogDescription}/>
 		<meta property="og:url" content={ogURL}/>
 		<meta property="og:image" content={ogImage}/>
+		<meta property="og:type" content={ogType}/>
 		<meta property="og:image:width" content="1200"/>
 		<meta property="og:image:height" content="630"/>
-		<meta property="og:type" content="website"/>
-		<meta property="og:site_name" content="이태윤의 블로그형 포트폴리오 사이트입니다."/>
+		<meta property="og:site_name" content="2taeyoon의 개인 블로그"/>
   </Helmet>
 );
 
