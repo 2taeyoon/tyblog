07965e784ff45ef8598d39969f100522554ff146
2taeyoon <2tae@kakao.com>
2025-06-05 23:48:26 +0900

    feat: Profile 컴포넌트에 falseActive props 추가 및 640px 이하일 때 이미지 변경 비활성화

--- src/components/ui/Profile.tsx
+++ src/components/ui/Profile.tsx
@@ -4,15 +4,28 @@ import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 
-export default function Profile() {
+interface AsideProps {
+	falseActive: () => void;
+}
+
+export default function Profile({falseActive}: AsideProps) {
 	const [imageSrc, setImageSrc] = useState("/images/profile.webp");
 
-	const handleMouseLeave = () => setImageSrc("/images/profile.webp");
-	const handleMouseEnter = () => setImageSrc("/images/profile2.webp");
+  const handleMouseEnter = () => {
+    if (typeof window !== "undefined" && window.innerWidth > 640) {
+      setImageSrc("/images/profile2.webp");
+    }
+  };
+
+  const handleMouseLeave = () => {
+    if (typeof window !== "undefined" && window.innerWidth > 640) {
+      setImageSrc("/images/profile.webp");
+    }
+  };
 
   return (
 		<div className="profile_wrap">
-			<Link href="/">
+			<Link href="/" onClick={falseActive}>
 				<div className="profile" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
 					<Image src={imageSrc} alt="profile" sizes="150px" fill style={{ objectFit: "cover" }}/>
 				</div>
